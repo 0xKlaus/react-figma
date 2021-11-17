20baad7b704bbb4b70361863f2964fe676bc7184
Hideki815 <moriyamahideki815@gmail.com>
2021-11-17 19:02:43 +0900

    Complet but Not responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+import Header from "./components/Header/Header";
+import Middle from "./components/Middle/Middle";
+import Footer from "./components/Footer/Footer";
+
+import BackgroundImg from "./assets/Background/background_img.png";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BackgroundDiv>
+      <Header />
+      <Middle />
+      <Footer />
+    </BackgroundDiv>
   );
-}
+};
 
 export default App;
+
+export const BackgroundDiv = styled.div`
+  height: 100%;
+  padding: 50px;
+  background-image: url(${BackgroundImg});
+  background-size: 100% 100%;
+`;
